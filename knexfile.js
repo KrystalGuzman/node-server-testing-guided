26c7e1ce753ed96bf0fdc93245292da7b19a32bf
Krystal Guzman <kryguzman@gmail.com>
2020-03-26 10:39:23 -0700

    added pg

--- knexfile.js
+++ knexfile.js
@@ -12,6 +12,20 @@ module.exports = {
       directory: "./data/seeds",
     },
   },
+  production: {
+    client: "pg",
+    connection: {
+      filename: process.env.DATABASE_URL,
+    },
+    useNullAsDefault: true,
+    migrations: {
+      directory: "./data/migrations",
+    },
+    seeds: {
+      directory: "./data/seeds",
+    },
+  },
+  
   testing: {
     client: "sqlite3",
     connection: {
